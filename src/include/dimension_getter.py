8266fcfb546b34066e7fac2a005dba6695029ccc
Rayver Aimar <rayver.munoz@ucsp.edu.pe>
2022-09-14 13:08:41 -0500

    Add dimension getter functional class

---
+++ src/include/dimension_getter.py
@@ -0,0 +1,54 @@
+import tkinter as tk
+from tkinter import font
+
+class DimensionGetter(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Dimension Getter")
+        self._cells = {}
+        self._pressed : int
+        self._create_menu()
+        self._create_board_display()
+        self._create_board_grid()
+    
+    def _create_menu(self):
+        menu_bar = tk.Menu(master=self)
+        self.config(menu=menu_bar)
+        file_menu = tk.Menu(master=menu_bar)
+        menu_bar.add_cascade(label="File", menu=file_menu)
+
+    def _create_board_display(self):
+        display_frame = tk.Frame(master=self)
+        display_frame.pack(fill=tk.X)
+        self.display = tk.Label(
+            master=display_frame,
+            text="Dimension of the board? ",
+            font=font.Font(size=28, weight="bold"),
+        )
+        self.display.pack()
+    
+    def _create_board_grid(self):
+        grid_frame = tk.Frame(master=self)
+        grid_frame.pack()
+        for row in range(2):
+            self.rowconfigure(row, weight=1, minsize=50)
+            self.columnconfigure(row, weight=1, minsize=75)
+            for col in range(2):
+                button = tk.Button(
+                    master=grid_frame,
+                    text=str(((row+1)*2) + (col + 1)),
+                    font=font.Font(size=36, weight="bold"),
+                    fg="black",
+                    width=3,
+                    height=2,
+                    highlightbackground="lightblue",
+                )
+                self._cells[button] = ((row+1)*2) + (col + 1)
+                button.bind("<ButtonPress-1>", self.pressed)
+                button.grid(row=row, column=col, padx=5, pady=5, sticky="nsew")
+
+    def pressed(self, event):
+        
+        clicked_btn = event.widget
+        self._pressed = self._cells[clicked_btn]
+        self.after(50, lambda:self.destroy())
